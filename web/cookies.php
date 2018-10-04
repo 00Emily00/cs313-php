38530c2c2ac4b0f4564f10378fe872b45a42af67
00Emily00 <emyzu97@hotmail.com>
2018-10-04 16:19:30 -0600

    cookie

--- web/cookies.php
+++ web/cookies.php
@@ -2,7 +2,18 @@
 ////set a cookie and give it a name and value then tell it when it should expire(i want it to expire 7 days from now)
 //setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
 
-$favorite = $_COOKIE("fav-text");
+//to display
+//$favorite = $_COOKIE["fav-text"];
+
+//you don't need to use cookies directly, you can use sessions that use cookies
+
+//before sending anything out, at the top of your page put
+session_start();
+
+if(isset($_SESSION["visit"]))
+    $_SESSION["visit"]++;
+
+$visit = $_SESSION["You have visited"]
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +22,7 @@ $favorite = $_COOKIE("fav-text");
         <title>Cookies</title>
     </head>
     <body>
-    <p>This is a cookies $favorite</p>
+    <!--<p>This is a cookies $favorite</p>-->
+        <p>$visit</p>
     </body>
 </html>
