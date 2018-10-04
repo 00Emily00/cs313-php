f540d1034d05497692c0e26d7e976a3a0982bf50
00Emily00 <emyzu97@hotmail.com>
2018-10-04 16:02:48 -0600

    cookie

--- web/cookies.php
+++ web/cookies.php
@@ -1,6 +1,6 @@
 <?php
 ////set a cookie and give it a name and value then tell it when it should expire(i want it to expire 7 days from now)
-setcookie(("fav-text"), "c is for cookie", time() + (86400 * 7);
+setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
 ?>
 
 <!DOCTYPE html>
