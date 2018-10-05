1a5455e053ee16b077f70821cac119dfe614d004
00Emily00 <emyzu97@hotmail.com>
2018-10-05 12:33:33 -0600

    shopping cart

--- web/confirmation.php
+++ web/confirmation.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Book Store</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="shoppingcart.css">
+</head>
+<body>
+    <h1>Book Store</h1>
 
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="cart.php">View Cart</a>
+        <a href="checkout.php">Checkout</a>
+    </div>
+    
+    <h1>Thank You for Shopping with Us</h1>
 <?php
 session_start();
     
 ?>
+    
+</body>
 </html>
