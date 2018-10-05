745564b8d806e7c407fd1a746c53f1935fa0b2bb
00Emily00 <emyzu97@hotmail.com>
2018-10-04 22:00:06 -0600

    02 Team Activity

---
+++ web/about-us.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bakery</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="about-us.php">About us</a></li>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="login.php">Home</a></li>
+
+    </ul>
+<?php
+  echo "<h1>Bakery: About Us</h1>";
+  echo "<p>Hello and welcome to our bakery. We love creating many delicious pastries for you and your family. This company was created to share our talents and bring a bit of happiness to everyone around us.</p><br>";
+  echo "We understand that life can get busy and stressful. We want to help you feel as stress free as possible. So we created this bakery that will provide you with home baked goods without the hassle.";
+?>
+</body>
+
+</html>
