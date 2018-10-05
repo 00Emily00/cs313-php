849136af77bf089cc8ae93ce93b6bd2b7a0a02e6
00Emily00 <emyzu97@hotmail.com>
2018-10-05 17:11:07 -0600

    Sohpping cart

--- web/browse.php
+++ web/browse.php
@@ -12,7 +12,7 @@
 <h1>Book Store</h1>
 
 <div class="topnav">
-  <a class="active" href="#home">Home</a>
+  <a class="browse.php" href="#home">Home</a>
   <a href="cart.php">View Cart</a>
   <a href="checkout.php">Checkout</a>
 </div>
@@ -20,7 +20,7 @@
     <form action="cart.php" method="post">
 	Select the books you would like to add to your cart<br>
     <input type="checkbox" name="book[]" value="1984">1984<br>
-    <input type="checkbox" name="book[]" value="To Kill a Mockingbird"><img src="1984.jpg">To Kill a Mockingbird<br>
+    <img src="1984.jpg"><input type="checkbox" name="book[]" value="To Kill a Mockingbird">To Kill a Mockingbird<br>
     <input type="checkbox" name="book[]" value="The Underground Railroad">The Underground Railroad<br>
     <input type="checkbox" name="book[]" value="Fahrenheit 451">Fahrenheit 451<br>
     <input type="checkbox" name="book[]" value="The Lovely Bones">The Lovely Bones<br>
@@ -30,7 +30,7 @@
     <input type="checkbox" name="book[]" value="Glass Castle">Glass Castle<br>
     <input type="checkbox" name="book[]" value="Hidden Figures">Hidden Figures<br>
     
-    <button type="submit">Submit</button>
+    <button type="submit">Checkout</button>
     </form>
 
 </body>
