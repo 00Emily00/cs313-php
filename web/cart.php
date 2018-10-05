074c1fc0819f5b110b9d768cdad7c95b7cb46141
00Emily00 <emyzu97@hotmail.com>
2018-10-05 11:31:16 -0600

    shopping cart

--- web/cart.php
+++ web/cart.php
@@ -2,10 +2,16 @@
 <html>
 <body>
 <?php
-    session_start();
+  session_start();  
+?>
+    <form action="cart.php" method"post">
+    
+    <?php
     foreach ($_POST["book"] as $item) {
-        echo $item.value . " ";
+        echo $item . " ";
+        echo "<button type='submit'>Delete</button><br>";
     }
 ?>
+    </form>
 </body>
 </html>
