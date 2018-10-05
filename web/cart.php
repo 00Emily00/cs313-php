7906df7ffff27732de0d60c24b8eacf886ab3211
00Emily00 <emyzu97@hotmail.com>
2018-10-05 11:50:51 -0600

    shopping cart

--- web/cart.php
+++ web/cart.php
@@ -3,6 +3,11 @@
 <body>
 <?php
   session_start();
+    //saving the books the user selected. item -variable
+    foreach ($_POST["book"] as $item) {
+    $_SESSION[$item] = $item;
+        echo "session $item";
+    }
 
     if(isset($_POST["delete"]))
         echo "deleted";
@@ -11,6 +16,7 @@
     
     <?php
     foreach ($_POST["book"] as $item) {
+        
         echo $item . " ";
         echo "<button type='submit' name='delete'>Delete</button><br>";
     }
