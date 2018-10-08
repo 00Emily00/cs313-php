b0737c5bdcb7524f7221cc0a0db76bc8d0553103
00Emily00 <emyzu97@hotmail.com>
2018-10-08 08:51:53 -0600

    shopping cart

--- web/browse.php
+++ web/browse.php
@@ -19,16 +19,16 @@
 
     <form action="cart.php" method="post">
 	Select the books you would like to add to your cart<br>
-    <input type="checkbox" name="book[]" value="1984">1984<br>
-    <img src="1984.jpg"><input type="checkbox" name="book[]" value="To Kill a Mockingbird">To Kill a Mockingbird<br>
-    <input type="checkbox" name="book[]" value="The Underground Railroad">The Underground Railroad<br>
-    <input type="checkbox" name="book[]" value="Fahrenheit 451">Fahrenheit 451<br>
-    <input type="checkbox" name="book[]" value="The Lovely Bones">The Lovely Bones<br>
-    <input type="checkbox" name="book[]" value="They Joy Luck Club">The Joy Luck Club<br>
-    <input type="checkbox" name="book[]" value="Siddhartha">Siddhartha<br>
-    <input type="checkbox" name="book[]" value="The Little Prince">The Little Prince<br>
-    <input type="checkbox" name="book[]" value="Glass Castle">Glass Castle<br>
-    <input type="checkbox" name="book[]" value="Hidden Figures">Hidden Figures<br>
+    <img src="1984.jpg"><input type="checkbox" name="book[]" value="1984">1984<br>
+    <img src="to%20kill%20a%20mockingbird.jpg"><input type="checkbox" name="book[]" value="To Kill a Mockingbird">To Kill a Mockingbird<br>
+    <img src="underground%20railroad.jpg"><input type="checkbox" name="book[]" value="The Underground Railroad">The Underground Railroad<br>
+    <img src="fahrenheit%20451.jpeg"><input type="checkbox" name="book[]" value="Fahrenheit 451">Fahrenheit 451<br>
+    <img src="lovely%20bones.jpg"><input type="checkbox" name="book[]" value="The Lovely Bones">The Lovely Bones<br>
+    <img src="joy%20luck%20club.jpg"><input type="checkbox" name="book[]" value="They Joy Luck Club">The Joy Luck Club<br>
+    <img src="siddhartha.jpg"><input type="checkbox" name="book[]" value="Siddhartha">Siddhartha<br>
+    <img src="little%20prince.jpg"><input type="checkbox" name="book[]" value="The Little Prince">The Little Prince<br>
+    <img src="glass%20castle.jpg"><input type="checkbox" name="book[]" value="Glass Castle">Glass Castle<br>
+    <img src="hidden%20figures.jpg"><input type="checkbox" name="book[]" value="Hidden Figures">Hidden Figures<br>
     
     <button type="submit">Checkout</button>
     </form>
