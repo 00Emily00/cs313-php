dd89d1271a6e2388a022ab8f8ef38a97a7947ed8
00Emily00 <emyzu97@hotmail.com>
2018-10-16 16:50:07 -0600

    Team Activity 05

--- web/TA05.php
+++ web/TA05.php
@@ -5,11 +5,11 @@
 <body>
     <h1>Scripture Resources</h1>
 <?php
-  $book ="";  
+  $book = $_GET['book'];  
 ?>   
 <?php
 include 'environmentvariable.php';
-    
+    $query = "SELECT * FROM scriptures WHERE book=:book";
   foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE $book=input') as $row)
 {
       echo '<p><b>' . $row['book'] . ' ';
@@ -17,10 +17,11 @@ include 'environmentvariable.php';
       echo $row['verse'] . '</b> - ';
       echo $row['content'] . '</p>';
 }  
+                
 ?>
     
-<form action="TA05.php">
-    SEARCH: <input type="text" name="input">
+<form action="TA05.php" method="get">
+    SEARCH: <input type="text" name="book">
     <button type="submit">Search</button>
 
 </form>
