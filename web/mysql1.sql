c356f8da1692ddd64221f032ba03725f7fdb2d8d
00Emily00 <emyzu97@hotmail.com>
2018-10-18 12:53:38 -0600

    sql project

--- web/mysql1.sql
+++ web/mysql1.sql
@@ -49,7 +49,7 @@ CREATE TABLE scriptures (
 
 );
 
-INSERT INTO scriptures(book, chapter, verse, content) VALUES ('John',1,5,' That was the true Light, which lighteth every man that cometh into the world.'), ('Doctrine and Covenants',88,49,'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'),('Doctrine and Covenants 93:28',93,28,'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.'),('Mosiah',16,9,'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
+INSERT INTO scriptures(book, chapter, verse, content) VALUES ('John',1,5,' That was the true Light, which lighteth every man that cometh into the world.'), ('Doctrine and Covenants',88,49,'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.'),('Doctrine and Covenants 93:',93,28,'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.'),('Mosiah',16,9,'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
 
 
 
