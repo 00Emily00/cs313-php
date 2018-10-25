2f118b28198ad91cc6df991f5dbd129fd0ab3e08
00Emily00 <emyzu97@hotmail.com>
2018-10-25 13:30:21 -0600

    project 1

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -23,12 +23,15 @@ $stmt->execute();
 $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);  
 ?>  
 <?php
+        foreach($rowsAnswers as $rowsAnswer)
+    {
+         echo '<input type="radio" name="animal2" value="' . $rowAnswer['answers'] . '">' . $rowAnswer['answers'] . '<br>';
+//        echo $rowsAnswer['answers'];
+    }
     foreach($rows as $row)
     {
         echo '<img src="' . $row['questions'] . '" >' . ' ' . '<input type="radio" name="animal" value="' . $row['answers'] . '">' . $row['answers'] . '<br>';  
         
-             echo '<input type="radio" name="animal2" value="' . $rowAnswer['answers'] . '">' . $rowAnswer['answers'] . '<br>';
-        debug_to_console("TEST");
 //        foreach($rowsAnswers as $rowsAnswer)
 //    {
 //         echo '<input type="radio" name="animal2" value="' . $rowAnswer['answers'] . '">' . $rowAnswer['answers'] . '<br>';
