f1d8c63e98fa52a8f8b0bd034075109352b36eec
00Emily00 <emyzu97@hotmail.com>
2018-10-31 14:59:49 -0600

    project 1 - sql/php

--- web/results.php
+++ web/results.php
@@ -1,8 +1,9 @@
 <?php
-//var_dump( $_POST);
- if (isset($_POST['submit'])) {
-        if(isset($_POST['radio'])) {
-                echo $_POST['radio'];
-        }
-    }
+var_dump( $_POST);
+
+if (isset($_POST['submit'])) {
+if(isset($_POST['radio']))
+{
+echo "You have selected :".$_POST['radio'];  //  Displaying Selected Value
+}
 ?>
