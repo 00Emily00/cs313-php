ba62f6434a3481d33497b886521cb5ff120b91b0
00Emily00 <emyzu97@hotmail.com>
2018-10-30 21:47:53 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -40,7 +40,7 @@ $stmt = $db->prepare("SELECT answers FROM answers ORDER BY RANDOM() LIMIT 3");
 $stmt->execute();
 $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);   
       foreach($rowsAnswers as $rowsAnswer) {
-         echo '<input type="radio" name="animal" value="' . $rowsAnswer['answers'] . '">' . $rowsAnswer['answers'] . '<br>';
+         echo '<input type="radio" name="' . $animal . '" value="' . $rowsAnswer['answers'] . '">' . $rowsAnswer['answers'] . '<br>';
 //       echo $rowsAnswer['answers'];
         }
     }
