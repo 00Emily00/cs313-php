1aed88e85f6206a24598624a8ddfc02d1bc5a8f5
00Emily00 <emyzu97@hotmail.com>
2018-10-30 19:18:57 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -20,10 +20,10 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php
     foreach($rows as $row)
     { //possib
-       
+       $i = 1;
         echo '<img src="' . $row['questions'] . '" >' . ' ' . '<br>'; //display image once
-        for($x = 0; $x < $rows['answers']; $x++) { 
-        echo '<input type="radio" name="' . $x['answers'] . '"  value="' . $row['answers'] . '">' . $row['answers'] . '<br>';
+//        for($x = 0; $x < $rows['answers']; $x++) { 
+        echo '<input type="radio" name="animal['i']"  value="' . $row['answers'] . '">' . $row['answers'] . '<br>';
         
     //select 3 answers as long as it's not a chicken in WHERE id etc :answerid
 $stmt = $db->prepare("SELECT answers FROM answers ORDER BY RANDOM() LIMIT 3");
@@ -31,12 +31,12 @@ $stmt = $db->prepare("SELECT answers FROM answers ORDER BY RANDOM() LIMIT 3");
 $stmt->execute();
 $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);   
       foreach($rowsAnswers as $rowsAnswer) {
-         echo '<input type="radio" name="' . $x['answers'] . '" value="' . $rowsAnswer['answers'] . '">' . $rowsAnswer['answers'] . '<br>';
+         echo '<input type="radio" name="animal['i']" value="' . $rowsAnswer['answers'] . '">' . $rowsAnswer['answers'] . '<br>';
 //       echo $rowsAnswer['answers'];
     }
     
     }
-    }
+  //  }
 ?>
 
 <form action="#" method="post">
