a58c45163a617cf2d0877586db20bad934e3e80d
00Emily00 <emyzu97@hotmail.com>
2018-10-30 19:50:58 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -22,7 +22,6 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     { //possib
         echo '<img src="' . $row['questions'] . '" >' . ' ' . '<br>'; //display image once
         
-        $row = $rows[$i]; 
         echo '<input type="radio" name="animal"  value="' . $row['answers'] . '">' . $row['answers'] . '<br>';
         
     //select 3 answers as long as it's not a chicken in WHERE id etc :answerid
