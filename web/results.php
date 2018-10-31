4fc97062df2f911ee6eddff3ef3281f291e7e2d9
00Emily00 <emyzu97@hotmail.com>
2018-10-31 16:43:21 -0600

    project 1 - sql/php

--- web/results.php
+++ web/results.php
@@ -1,5 +1,5 @@
 <?php
-var_dump( $_POST);
+//var_dump( $_POST);
 //
 //if (isset($_POST['submit'])) {
 //if(isset($_POST['radio']))
@@ -10,7 +10,7 @@ var_dump( $_POST);
 
 
 foreach($_POST as $key => $value ) { //get keys and values from post and display them
-echo $key;
+pring_r($key);
 print_r($avalue);
 }
 ?>
