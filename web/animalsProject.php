6f67a1f78bd87359a29d700630afe544f8223409
00Emily00 <emyzu97@hotmail.com>
2018-10-31 10:30:26 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -39,14 +39,23 @@ $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);
          echo '<input type="radio" name="' . $animal . '" value="' . $rowsAnswer['answers'] . '">' . $rowsAnswer['answers'] . '<br>';
         }
     }
-    
-    echo "<input type='submit' name='submit' value='Get Results'/>"
     if (isset($_POST['submit'])) {
-  if(isset($_POST['radio'])) {
-      echo "You got these correct: " . $_POST['radio'];
-  }
-}
+        if(isset($_POST['radio'])) {
+            
+    foreach($_POST['radio'] as $select) {
+        echo $select;
+    }
+        }
+    }
+    
+//    echo "<input type='submit' name='submit' value='Get Results'/>"
+//    if (isset($_POST['submit'])) {
+//        if(isset($_POST['radio'])) {
+//      echo "You got these correct: " . $_POST['radio'];
+//  }
+//}
 ?>
+
 <!--    <input type="submit" name="submit" value="Get Results"/>-->
 </form>
 
