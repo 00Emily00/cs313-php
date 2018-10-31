1afb911425d843402b8714e2102b230d6d3dbf92
00Emily00 <emyzu97@hotmail.com>
2018-10-31 11:17:50 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -31,7 +31,8 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo '<input type="radio" name="' . $animal . '" value="' . $row['answers'] . '">' . $row['answers'] . '<br>';
         
     //select 3 answers as long as it's not a chicken in WHERE id etc :answerid
-$stmt = $db->prepare("SELECT answers FROM answers WHERE qid NOT IN(SELECT q.questions, a.answers FROM questions q JOIN questionanswer qa ON q.questionId=qa.qid JOIN answers a ON qa.aid=a.answerId) ORDER BY RANDOM() LIMIT 3");
+//    $stmt = $db->prepare("SELECT answers FROM answers WHERE qid NOT IN(SELECT q.questions, a.answers FROM questions q JOIN questionanswer qa ON q.questionId=qa.qid JOIN answers a ON qa.aid=a.answerId) ORDER BY RANDOM() LIMIT 3");
+$stmt = $db->prepare("SELECT answers FROM answers ORDER BY RANDOM() LIMIT 3");
 //id!= :answerId 
 $stmt->execute();
 $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);   
