f3b949edb0e25f9221f546e714b7c73dd86e4caa
00Emily00 <emyzu97@hotmail.com>
2018-10-30 18:32:40 -0600

    project 1 - sql/php

--- web/animalsProject.php
+++ web/animalsProject.php
@@ -25,7 +25,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo '<input type="radio" name="animal" value="' . $row['answers'] . '">' . $row['answers'] . '<br>';
         
     //select 3 answers as long as it's not a chicken in WHERE id etc :answerid
-$stmt = $db->prepare("SELECT answers FROM answers ORDER BY RANDOM() LIMIT 3");
+$stmt = $db->prepare("SELECT answers FROM answers WHERE aid != $row['answers'] ORDER BY RANDOM() LIMIT 3");
 //id!= :answerId 
 $stmt->execute();
 $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);   
@@ -36,7 +36,16 @@ $rowsAnswers = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     }
 ?>
+
+<form action="#" method="post">
+    <input type="submit" name="submit" value="Get Results"/>
+</form>
     
+<?php
+  if(isset($_POST['submit'])) {
+      echo $row['answers'];
+  }  
+?>
     
 
     
