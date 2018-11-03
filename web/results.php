0891d1fa492056a7819082098443d5f4e3d4d4e0
00Emily00 <emyzu97@hotmail.com>
2018-11-03 11:55:35 -0600

    project 1 - sql/php

--- web/results.php
+++ web/results.php
@@ -1,21 +1,7 @@
 <?php
 require ('dbConnect.php');
 $db = get_db();
-//var_dump( $_POST);
-//
-//if (isset($_POST['submit'])) {
-//if(isset($_POST['radio']))
-//{
-//echo "You have selected :".$_POST['radio'];  //  Displaying Selected Value
-//}
-//}
 
-//var_dump($_POST) shows an array with keys and values
-//do a foreach
-//pull all rows
-
-//call the questionanswer
-//get user input
 $stmt = $db->prepare("SELECT qId, aId FROM questionanswer");
 //id!= :answerId 
 $stmt->execute();
@@ -33,13 +19,13 @@ foreach($rows as $row) {
 echo "<h1>CONGRATULATIONS!!! YOU GOT $points Points!"
 
 
-
-
-//foreach($_POST as $key => $value ) { //get keys and values from post and display them
-//echo $key;
-//echo $value;
-//}
-
-
-//to check if they got the right answer, compare the one user checked and picture with the aid=qid. If the user answer with the question does not exist in the database, then dont add a point.
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Learn Your Animals</title>
+</head>
+<body>
+</body>
+</html>
+
