e5e596c9effb0617f70a273a1bcf064e7e8ac35c
00Emily00 <emyzu97@hotmail.com>
2018-11-03 11:56:43 -0600

    project 1 - sql/php

--- web/results.php
+++ web/results.php
@@ -24,6 +24,8 @@ echo "<h1>CONGRATULATIONS!!! YOU GOT $points Points!"
 <html>
 <head>
     <title>Learn Your Animals</title>
+    <link rel="stylesheet" href="animalsProject.css">
+
 </head>
 <body>
 </body>
